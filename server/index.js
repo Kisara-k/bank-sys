0ead3770ef71888a87d1dd74d488a34d08b9ea1e
Vidura Kahandawa <viduramalshan185@gmail.com>
2024-09-06 18:07:50 +0530

    change data base detail
    After creating database

--- server/index.js
+++ server/index.js
@@ -10,20 +10,35 @@ const db=mysql.createConnection({
     user:"root",
     host:"localhost",
     password:"20020927",
-    database:"bank",
+    database:"banksys",
 });
 
 app.post("/log",(req,res)=>{
     const username=req.body.username;
     const passkey=req.body.passkey;
 
-    db.query("SELECT * FROM customer WHERE customer_ID=? AND passkey=?",
-        [username,passkey],
+    db.query("SELECT * FROM customer join individualcustomer on customer.CustomerID=individualcustomer.CustomerID WHERE customer.CustomerID=? AND Password=?",
+        [username,passkey,username,passkey],
         (err,result)=>{
             if(err){
                 console.log(err);
             }else{
-                res.send(result);
+                if(result.length!=0){
+                    res.send(result);
+                }
+                else{
+                    db.query("SELECT * FROM customer join organizationcustomer on customer.CustomerID=organizationcustomer.CustomerID WHERE customer.CustomerID=? AND Password=?",
+                        [username,passkey],
+                        (err,result)=>{
+                            if(err){
+                                console.log(err);
+                            }else{
+                                res.send(result);
+                            }
+                        }
+                    )
+                }
+                
             }
         }
     )

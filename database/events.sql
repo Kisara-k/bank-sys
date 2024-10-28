a3d01fa1076d4c1a37b4abed17fd42ece4c657d4
Sandali Jayawardhana <150525955+Sandali0726@users.noreply.github.com>
2024-10-28 17:51:43 +0530

    Update schedule for monthly rates event

--- database/events.sql
+++ database/events.sql
@@ -9,7 +9,7 @@ DO
 
 ----------------update monthly rates
 CREATE EVENT update_monthly_rates 
-ON SCHEDULE EVERY 1 MINUTE
+ON SCHEDULE EVERY 30 DAY 
 STARTS '2024-10-13 00:00:00' ON COMPLETION PRESERVE 
 ENABLE 
 DO UPDATE account 
